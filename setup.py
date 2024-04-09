049c8c88e2db2d3b1a817b9d58f36af54f804b0a
Deivid Hugo <deivid.hugo@dmslog.com>
2024-04-09 19:00:38 -0300

    Init lib

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-formsets-inside-form',
+    version='1.0',
+    description='A library to handle formsets within forms in Django',
+    author='Deivid Hugo',
+    author_email='deividhugoof@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=2.0',
+    ],
+)
